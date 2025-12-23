31000434d78ea54fd9fa02162a98bf626bb2ba1c
Ahmad Sulehari <142904542+ahmadsulehari@users.noreply.github.com>
2025-12-23 12:01:47 +0500

    Added workflow to run on PR creation for main branch and execute test for the app routes (#1)
    * update: trying to run test in github actions
    
    * update: added permission to scripts inside workflow
    
    * Fix URL path in test_say_hello function
    
    * update: summary script can now run without args
    
    * fix: fixed name for summarize script
    
    * updated workflow to run only when a PR is created for main branch

--- scripts/summarize_results.sh
+++ scripts/summarize_results.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-log_file_path=$WORKING_DIR/logs/$1
+
 
 if [[ -z $1 ]]; then
-    echo "Usage: $0 <log_file_name>"
-    exit 1
+    echo "No arg provided trying to locate the first log file in logs/ directory."
+    log_file_name=$(find logs/ -type f -name pytest_*.log | head -n 1 | awk -F '/' '{print $2}')
+    if [[ -z $log_file_name ]]; then
+        echo "No log file found in logs/ directory."
+        echo "Usage: $0 <log_file_name>"
+        exit 1
+    else
+        echo "Using log file: $log_file_name"
+    fi
+else
+    log_file_name=$1
 fi
+
+log_file_path=$WORKING_DIR/logs/$log_file_name
+echo "Reading log file: $log_file_path"
 
 if [[ -s $log_file_path ]]; then
     total_tests=$(grep -o 'collected [0-9]* items' "$log_file_path" | awk '{print $2}')
